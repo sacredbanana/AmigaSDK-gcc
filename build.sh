b34e33c93ae747e9c887f3f2a133f345fcb5b241
Cameron Armstrong (Nightfox) <cameronscottarmstrong@gmail.com>
2023-11-21 20:35:56 +0800

    when pushing, build both arm and x86 images for m68k. Unfortunately PPC is only available in x86 at this stage

--- build.sh
+++ build.sh
@@ -10,10 +10,14 @@ if [[ -z "${NAME}" ]]; then
     NAME="sacredbanana/amiga-compiler"
 fi
 
-docker build -t ${NAME}:m68k-amigaos --rm -f Dockerfile.m68k .
 docker build -t ${NAME}:ppc-amigaos --rm -f Dockerfile.ppc .
 
 if [[ "${PUSH}" == "1" ]]; then
-  docker push ${NAME}:m68k-amigaos
   docker push ${NAME}:ppc-amigaos
+  docker buildx rm amigasdkbuilder || true
+  docker buildx create --name amigasdkbuilder --use
+  docker buildx build --platform linux/amd64,linux/arm64 -t ${NAME}:m68k-amigaos --rm -f Dockerfile.m68k --push .
+  docker buildx rm amigasdkbuilder
+else
+  docker build -t ${NAME}:m68k-amigaos --rm -f Dockerfile.m68k .
 fi
