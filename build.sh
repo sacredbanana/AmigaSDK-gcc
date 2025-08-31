174a75f42d68b86cb087d53c6e23602973992add
Cameron Armstrong (Nightfox) <cameronscottarmstrong@gmail.com>
2025-08-31 16:35:57 +0800

    Support building ARM version of MorphOS image

--- build.sh
+++ build.sh
@@ -10,16 +10,15 @@ if [[ -z "${NAME}" ]]; then
     NAME="sacredbanana/amiga-compiler"
 fi
 
-docker build -t ${NAME}:ppc-morphos --rm -f Dockerfile.ppc-morphos .
-
 if [[ "${PUSH}" == "1" ]]; then
-  docker push ${NAME}:ppc-morphos
   docker buildx rm amigasdkbuilder || true
   docker buildx create --name amigasdkbuilder --use
   docker buildx build --platform linux/amd64,linux/arm64 -t ${NAME}:m68k-amigaos --rm -f Dockerfile.m68k-amigaos --push .
   docker buildx build --platform linux/amd64,linux/arm64 -t ${NAME}:ppc-amigaos --rm -f Dockerfile.ppc-amigaos --push .
+  docker buildx build --platform linux/amd64,linux/arm64 -t ${NAME}:ppc-morphos --rm -f Dockerfile.ppc-morphos --push .
   docker buildx rm amigasdkbuilder
 else
   docker build -t ${NAME}:m68k-amigaos --rm -f Dockerfile.m68k-amigaos .
   docker build -t ${NAME}:ppc-amigaos --rm -f Dockerfile.ppc-amigaos .
+  docker build -t ${NAME}:ppc-morphos --rm -f Dockerfile.ppc-morphos .
 fi
